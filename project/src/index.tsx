08e716edb76e501160a4f0e2f479d1f893b78145
Helen <helengriva@gmail.com>
2022-02-18 17:48:38 +0300

    Создаст компоненты приложения

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const catalogGenres = [
+  {
+    href: 'all',
+    name: 'All genres',
+  },
+];
+
+const Setting = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  DATE: 2014,
+  CATALOG_GENRES: catalogGenres,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilmTitle = {Setting.TITLE}
+      promoFilmGenre = {Setting.GENRE}
+      promoFilmYear = {Setting.DATE}
+      catalogGenres = {Setting.CATALOG_GENRES}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
