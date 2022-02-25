2e2249d3a51ae74f79a3c48d082b15ed220f026a
Helen <helengriva@gmail.com>
2022-02-25 14:14:18 +0300

    Выделит в компонент страницу фильма, добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainScreen from '../../pages/main-screen/main-screen';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import MovieScreen from '../../pages/movie-screen/movie-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
 
 type Genre = {
   href: string;
@@ -22,14 +29,47 @@ type AppScreenProps = {
 
 function App({mainFilmTitle, mainFilmGenre, mainFilmYear, catalogGenres, filmCards, activeGenre}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen
-      mainFilmTitle = {mainFilmTitle}
-      mainFilmGenre = {mainFilmGenre}
-      mainFilmYear = {mainFilmYear}
-      catalogGenres = {catalogGenres}
-      filmCards = {filmCards}
-      activeGenre = {activeGenre}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <MainScreen
+              mainFilmTitle = {mainFilmTitle}
+              mainFilmGenre = {mainFilmGenre}
+              mainFilmYear = {mainFilmYear}
+              catalogGenres = {catalogGenres}
+              filmCards = {filmCards}
+              activeGenre = {activeGenre}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={
+            <SignInScreen />
+          }
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <MyListScreen filmCards = {filmCards} />
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MovieScreen />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen checkedRating='5'/>}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -5,7 +5,6 @@ function SignInScreen(): JSX.Element {
     <div className="user-page">
       <header className="page-header user-page__head">
         <Logo />
-
         <h1 className="page-title user-page__title">Sign in</h1>
       </header>
 
