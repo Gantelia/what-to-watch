3a380e67d53dde1762ddceede169fcbb8d4572db
Helen <helengriva@gmail.com>
2022-04-01 21:18:04 +0300

    Исправит ошибку в логике компонента MyListButton

--- project/src/components/my-list-button/my-list-button.tsx
+++ project/src/components/my-list-button/my-list-button.tsx
@@ -1,10 +1,9 @@
-import { Link } from 'react-router-dom';
-import { AppRoute, FavoriteStatus } from '../../const';
+import { FavoriteStatus } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeFavoriteAction } from '../../store/api-actions/api-film-actions';
 import { FilmInfo } from '../../types/films';
-import { isAuthorized, isFavorite } from '../../utils';
-/*eslint-disable*/
+import { isFavorite } from '../../utils';
+
 type MyListButtonProps = {
     favoriteFilm: FilmInfo;
 }
@@ -13,34 +12,16 @@ function MyListButton({favoriteFilm}: MyListButtonProps): JSX.Element {
   const dispatch = useAppDispatch();
 
   const {currentFavorite} = useAppSelector(({FAVORITE}) => FAVORITE);
-  const {authorizationStatus} = useAppSelector(({USER}) => USER);
 
-  const getFavoriteStatus = () => {
-    if (isFavorite(currentFavorite, favoriteFilm)) {
-      return currentFavorite?.isFavorite? FavoriteStatus.NotFavorite : FavoriteStatus.Favorite;
-    }
-    return favoriteFilm.isFavorite? FavoriteStatus.NotFavorite : FavoriteStatus.Favorite;
-  };
+  const favoriteStatus = isFavorite(currentFavorite, favoriteFilm)? FavoriteStatus.NotFavorite : FavoriteStatus.Favorite;
+
 
   const handleFavoriteClick = () => {
     dispatch(changeFavoriteAction({
       id: favoriteFilm.id,
-      status: getFavoriteStatus(),
+      status: favoriteStatus,
     }));
   };
-
-  if (!isAuthorized(authorizationStatus)) {
-    return (
-      <Link to={AppRoute.SignIn}>
-        <button className="btn btn--list film-card__button" type="button">
-          <svg viewBox="0 0 19 20" width="19" height="20">
-            <use xlinkHref="#add"></use>
-          </svg>
-          <span>My list</span>
-        </button>
-      </Link>
-    );
-  }
 
   return (
     <button className="btn btn--list film-card__button" type="button"
