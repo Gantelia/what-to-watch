5baf9637d3b24e61766bb6ee1c660811bc4c9d85
Helen <helengriva@gmail.com>
2022-04-06 23:05:26 +0300

    Добавит тесты для асинхронных действий авторизации

--- project/src/store/api-actions/api-auth-actions/api-auth-actions.test.ts
+++ project/src/store/api-actions/api-auth-actions/api-auth-actions.test.ts
@@ -4,9 +4,10 @@ import { State } from '../../../types/state';
 import thunk from 'redux-thunk';
 import { Action, ThunkDispatch } from '@reduxjs/toolkit';
 import {configureMockStore} from '@jedmao/redux-mock-store';
-import { checkAuthAction } from './api-auth-actions';
+import { checkAuthAction, loginAction, logoutAction } from './api-auth-actions';
 import { APIRoute } from '../../../const';
 import { getUserData, requireAuthorization } from '../../user-process/user-process';
+import { AuthData } from '../../../types/auth-data';
 
 describe('Async auth actions', () => {
   const api = createAPI();
@@ -19,7 +20,7 @@ describe('Async auth actions', () => {
     ThunkDispatch<State, typeof api, Action>
     >(middlewares);
 
-  it('should dispatch "requireAuthorization" and "getUserData" when server returns 200', async () => {
+  it('should dispatch RequireAuthorization and GetUserData when server returns 200', async () => {
     const store = mockStore();
     mockAPI
       .onGet(APIRoute.Login)
@@ -33,6 +34,49 @@ describe('Async auth actions', () => {
 
     expect(actions).toContain(requireAuthorization.toString());
     expect(actions).toContain(getUserData.toString());
-
   });
+
+  it('should dispatch RequireAuthorization and GetUserData when POST/login', async () => {
+    const fakeUser: AuthData = {login: 'test@test.ru', password: 'dfg'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'secret'});
+
+    const store = mockStore();
+
+    expect(store.getActions()).toEqual([]);
+
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+    expect(actions).toContain(getUserData.toString());
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('wtw-token', 'secret');
+  });
+
+  it('should dispatch RequireAuthorization when DELETE/logout', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('wtw-token');
+  });
+
+
 });
